aa1c57a8eb6ba8389fca7f16248992ba062f5b7e
Marcos Filipi <133679958+filipiayres@users.noreply.github.com>
2023-12-11 17:53:43 -0300

    Delete MVP-engenharia-de-software directory

--- MVP-engenharia-de-software/api cardio/schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-from schemas.apanhado_schema import ApanhadoSchema,  ApanhadoBuscaSchema, ApanhadoViewSchema, ApanhadoDelSchema, \
-                                    apresenta_apanhados, apresenta_apanhado
-                                        
-from schemas.error_schema import ErrorSchema
-                                    
